10839e06cc94d572d0d8d1f6d3a336c8eb3682ac
Aggio98 <nigelrizzo@gmail.com>
2022-09-05 16:49:13 +0200

    Tim

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { HomePage, DetailsPage, ShopPage } from "./pages";
+import { NavBar } from "./components";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/shop" element={<ShopPage />}></Route>
+        <Route path="/details/:id" element={<DetailsPage />}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+// import { NavLink } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <div className="container">
+      <div className="greenbox"></div>
+      <div className="redbox"></div>
+    </div>
+  );
+};
+
+export { NavBar };
