95bc2167706cbcc4239c4d1be306b8789607da94
TDRAGSTRA <TIMBO040@GMAIL.COM>
2022-09-09 11:26:24 +0200

    footer fix

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -1,35 +1,35 @@
 import Card from "react-bootstrap/Card";
 import { NavLink } from "react-router-dom";
-import StarRatings from "./react-star-ratings";
+// import StarRatings from "./react-star-ratings";
 
 export default function ProductCard(props) {
-  return (
-    <Card style={{ maxHeight: "300px", minHeight: "200px" }}>
-      <Card.Body
-        style={{
-          display: "flex",
-          flexDirection: "row",
-        }}
-      >
-        <Card.Img
-          style={{
-            width: "150px",
-            height: "150px",
-          }}
-          variant="top"
-          src={props.image}
-          alt=""
-        />
-        <div style={{ display: "flex", flexDirection: "column" }}>
-          <NavLink to={`/details/${props.id}`}>
-            <Card.Title>{props.name}</Card.Title>
-          </NavLink>
-          <div>
-            <Card.Text>€{props.price}</Card.Text>
-            <Card.Text>{props.rating}</Card.Text>
-          </div>
-        </div>
-      </Card.Body>
-    </Card>
-  );
+	return (
+		<Card style={{ maxHeight: "300px", minHeight: "200px" }}>
+			<Card.Body
+				style={{
+					display: "flex",
+					flexDirection: "row",
+				}}
+			>
+				<Card.Img
+					style={{
+						width: "150px",
+						height: "150px",
+					}}
+					variant="top"
+					src={props.image}
+					alt=""
+				/>
+				<div style={{ display: "flex", flexDirection: "column" }}>
+					<NavLink to={`/details/${props.id}`}>
+						<Card.Title>{props.name}</Card.Title>
+					</NavLink>
+					<div>
+						<Card.Text>€{props.price}</Card.Text>
+						<Card.Text>{props.rating}</Card.Text>
+					</div>
+				</div>
+			</Card.Body>
+		</Card>
+	);
 }
